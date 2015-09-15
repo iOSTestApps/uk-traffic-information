2ff347e9ab16ba566e873646bd17c620043e30c2
Alex Chesters <alex.chesters@bbc.co.uk>
2015-09-15 18:02:45 +0100

    Initial commit of project

---
+++ uk-traffic-information/uk-traffic-information/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  uk-traffic-information
+//
+//  Created by Alex Chesters on 15/09/2015.
+//  Copyright © 2015 Alex Chesters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
