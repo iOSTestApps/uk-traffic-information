6cd30f1f3eac13e203d1b88052621ca91213c6da
Alex Chesters <alex.chesters@bbc.co.uk>
2015-09-15 19:02:06 +0100

    Adding side menu

---
+++ uk-traffic-information/uk-traffic-information/MainNavigationController.swift
@@ -0,0 +1,47 @@
+//
+//  MainNavigationController.swift
+//  uk-traffic-information
+//
+//  Created by Alex Chesters on 15/09/2015.
+//  Copyright © 2015 Alex Chesters. All rights reserved.
+//
+
+import UIKit
+
+class MainNavigationController: ENSideMenuNavigationController, ENSideMenuDelegate {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sideMenu = ENSideMenu(sourceView: self.view, menuViewController: MenuTableViewController(), menuPosition:.Left)
+        //sideMenu?.delegate = self //optional
+        sideMenu?.menuWidth = 180.0 // optional, default is 160
+        //sideMenu?.bouncingEnabled = false
+        
+        // make navigation bar showing over side menu
+        view.bringSubviewToFront(navigationBar)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - ENSideMenu Delegate
+    func sideMenuWillOpen() {
+        print("sideMenuWillOpen")
+    }
+    
+    func sideMenuWillClose() {
+        print("sideMenuWillClose")
+    }
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+}
