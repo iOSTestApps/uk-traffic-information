5f0f0bcece162c2e0c6f637fa0704778cf2d050c
Alex Chesters <alex.chesters@bbc.co.uk>
2015-09-15 20:22:36 +0100

    Removing unneeded code

--- uk-traffic-information/uk-traffic-information/MainNavigationController.swift
+++ uk-traffic-information/uk-traffic-information/MainNavigationController.swift
@@ -13,17 +13,11 @@ class MainNavigationController: ENSideMenuNavigationController, ENSideMenuDelega
         super.viewDidLoad()
         
         sideMenu = ENSideMenu(sourceView: self.view, menuViewController: MenuTableViewController(), menuPosition:.Left)
-        //sideMenu?.delegate = self //optional
         sideMenu?.menuWidth = 180.0 // optional, default is 160
-        //sideMenu?.bouncingEnabled = false
+        sideMenu?.bouncingEnabled = false
         
         // make navigation bar showing over side menu
         view.bringSubviewToFront(navigationBar)
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     // MARK: - ENSideMenu Delegate
