bba9bb5eabf8009ddf90702901f1574205af8e78
Alex Chesters <alex.chesters@bbc.co.uk>
2015-09-26 01:05:39 +0100

    #4 Adding annotation to map

--- uk-traffic-information/uk-traffic-information/DetailViewController.swift
+++ uk-traffic-information/uk-traffic-information/DetailViewController.swift
@@ -19,8 +19,14 @@ class DetailViewController: UIViewController, MKMapViewDelegate {
         super.viewDidLoad()
         mapView.delegate = self
         let region = MKCoordinateRegionMakeWithDistance(selectedEvent.location.coordinate, 1000 * 2.0, 1000 * 2.0)
+        let point = MKPointAnnotation()
+        point.coordinate = selectedEvent.location.coordinate
+        point.title = selectedEvent.road
+        point.subtitle = selectedEvent.description
+        mapView.addAnnotation(point)
         mapView.setRegion(region, animated: true)
         mapView.mapType = MKMapType.Hybrid
+        mapView.selectAnnotation(point, animated: true)
     }
 
 }
